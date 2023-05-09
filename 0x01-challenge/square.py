cc93dfdd9d9266e2292d65dc63c367f64ce3a38f
Ogoobaby <ogochukwumarygloria16@gmail.com>
2023-05-09 09:07:00 -0700

    New Tasks

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
+"""Plays with Square Geometry."""
 
-class Square:
+
+class square():
+    """Defines a square."""
+
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """Initialize a square."""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area(self):
-        """Area of the square"""
-        return self.width * self.width
+    def area_of_my_square(self):
+        """Area of the square."""
+        return self.width * self.height
 
-    def perimeter(self):
-        """Perimeter of the square"""
+    def permiter_of_my_square(self):
+        """Perimeter of square."""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Return string representation of class."""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
-    print(s.area())
-    print(s.perimeter())
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
